90c3ea4557b24053a5c07c4045f0d4abd5df49e7
Nick Pendery <npendery@gmail.com>
2023-12-26 15:06:22 -0800

    django project init

---
+++ loft_temperature_feed_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoftTemperatureFeedAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'loft_temperature_feed_app'
---
+++ loft_temperature_feed_challenge/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for loft_temperature_feed_challenge project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loft_temperature_feed_challenge.settings')
+
+application = get_asgi_application()
