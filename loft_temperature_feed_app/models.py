1c39a295caab5c9bf5e78be0004a0a727bcfcc01
Nick Pendery <npendery@gmail.com>
2024-01-01 13:21:28 -0700

    Fix timestampe editable

--- loft_temperature_feed_app/models.py
+++ loft_temperature_feed_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Temperature(models.Model):
     temperature = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return f"{self.temperature} at {self.timestamp}"  
