4eccc5fc4acb0cf726c11a0e90ae7b0f0306f505
Nick Pendery <npendery@gmail.com>
2024-01-01 16:19:08 -0700

    Get one test in

--- loft_temperature_feed_app/tests.py
+++ loft_temperature_feed_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from loft_temperature_feed_app.models import Temperature
+
+class TemperatureModelTests(TestCase):
+    def test_temperature_str(self):
+        temperature = Temperature(temperature=20.0, timestamp="2021-10-10 10:10:10")
+        self.assertEqual(str(temperature), "20.0 at 2021-10-10 10:10:10")
