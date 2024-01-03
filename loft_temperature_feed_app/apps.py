1c8da8a6ee0927ce08520bb9d0a0f634b15d16d6
Nick Pendery <npendery@gmail.com>
2024-01-02 20:56:52 -0700

    Working feed mutator with celery

--- loft_temperature_feed_app/apps.py
+++ loft_temperature_feed_app/apps.py
@@ -4,7 +4,3 @@ class LoftTemperatureFeedAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loft_temperature_feed_app'
 
-    def ready(self):
-        from loft_temperature_feed_app.tasks import ingest_temperatures
-        ingest_temperatures.delay()
-        pass
